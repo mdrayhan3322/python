2cbeb714e8a8ab6714d2f400f2b35f89e154e342
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-07-02 00:34:01 +0600

    operator

---
+++ practice/.py
@@ -0,0 +1,20 @@
+
+a = 20
+b = 20
+
+# output True or False
+print(' 1. a == b is = ',a==b)
+# output True or False
+print(' 2. a != b is = ',a!=b)
+print(' 3. a <= b is = ',a<=b)
+print(' 4. a >= b is = ',a>=b)
+
+# a = 20, b = 5
+# a**b = 20*20*20*20*20
+# k = a**b
+# print('a**b is = ',k)
+# c = a/b
+# print('a/b is = ',c)
+# d = a//b
+# print('a//b is = ',d)
+
