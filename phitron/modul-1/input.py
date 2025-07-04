394a25af9334bdea75312881d921e9dd52c32c04
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-07-04 23:25:01 +0600

    add number

---
+++ phitron/modul-1/input.py
@@ -0,0 +1,17 @@
+
+# input()
+# input("give me sum money")
+# money = input('give me sum money : ')
+# print('here is your money ',money)
+
+
+first_money = input('nijam bondu kisutaka de :')
+second_money = input("arafat bondu kisu taka de :")
+print(first_money,second_money)
+total = first_money + second_money
+print(type(total))
+first_money_int = int(first_money)
+second_money_int = int(second_money)
+total_int_money = first_money_int + second_money_int
+print(total_int_money)
+# print("total money I got : ",total)
---
+++ phitron/modul-1/variable.py
@@ -0,0 +1,12 @@
+
+age = 25
+integer_rate = 3.5
+name = "Rayhan"
+frind_name= 'Nijem'
+tr = True
+fl = False
+print(tr , age)
+sum = f"age additin {age} and integer_rate {integer_rate}"
+print(sum)
+
+print('mahamud ali'+'chasa badam')
