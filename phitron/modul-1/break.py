a2bf8b7755f8778ee87d2a8b99db9ed79bf9441c
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-08-04 15:50:50 +0600

    break

---
+++ phitron/modul-1/break.py
@@ -0,0 +1,7 @@
+
+b = 1
+while b <= 39 :
+    print(b)
+    b += 1
+    if b == 5:
+        break
---
+++ phitron/modul-1/while_loop.py
@@ -0,0 +1,14 @@
+
+# number = 1
+# while number < 5:
+#     print(number)
+#     number = number + 1
+
+a = 1 
+while a <=6 :
+    print(a)
+    a += 1
+    if a == 4 :
+        break
+
+    
