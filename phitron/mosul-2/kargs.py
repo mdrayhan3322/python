58c10731d3ab4b31a60b773aa859be8af9925bc2
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-09-09 12:04:54 +0600

    ching code

--- phitron/mosul-2/kargs.py
+++ phitron/mosul-2/kargs.py
@@ -7,9 +7,20 @@ name = full_name("md","rayhan")
 print(name)
 
  """
-
+# ---------------------------
+""" 
 def ching_name(first_name,last_name):
     name = f"{first_name} {last_name}"
     return name
 name = ching_name(last_name="rayhan",first_name='Md')
+print(name)
+
+ """
+
+# --------------
+
+def a(last,title,*first):
+    name = f"{title} {last} {first}"
+    return name
+name = a(last="rayhan",title="eng")
 print(name)
