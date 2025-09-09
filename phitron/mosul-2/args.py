08cb5fa4a2af42ec61dcaa08ce4a02be07d6b5a9
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-09-09 09:09:55 +0600

    args

--- phitron/mosul-2/args.py
+++ phitron/mosul-2/args.py
@@ -1,6 +1,20 @@
 
+
+
+""" 
 def sum (num1,num2,num3=0 , num4=0,num5=0):
     result = num1+num2+num3+num4+num5
     return result
 ans = sum(29,29,2)
 print(ans)
+
+ """
+# agrs
+def a(*number):
+    print(number)
+    sum = 0
+    for a_valu in number:
+       sum = sum+a_valu
+    return sum
+total = a(3,3,5,3,32) 
+print(total) 
