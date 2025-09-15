5aed92f30bcdf28b337edbe29329ae9e0bc5cfe9
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-09-15 16:47:37 +0600

    if condition

---
+++ dipti/input.py
@@ -0,0 +1,8 @@
+
+give = int(input("gIve me a number "))
+if(give > 0):
+    print("gibe is a positive numbe")
+else:
+    print("give is nagative number")
+print("'This statement always executes")
+
