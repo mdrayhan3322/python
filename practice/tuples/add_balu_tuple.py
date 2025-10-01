42bb7329d5cd83c7a652589c8b2c271619369c9b
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-10-01 11:30:00 +0600

    tuple add element help in list

---
+++ practice/tuples/add_balu_tuple.py
@@ -0,0 +1,9 @@
+
+tp = ("rayahn","mahamud","korem") # tuple 
+print(tp) 
+a = list(tp) # convert a list 
+print(a)
+a.append("jahid") # add a list a element
+#print(a)
+new_tuple = tuple(a) # convert tuple 
+print(new_tuple) # print tupele 
