238b28b2862d9b4ff8ab5162a137b377c16a497d
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-10-03 12:11:33 +0600

    list convert set

---
+++ practice/dictionary/informanr.py
@@ -0,0 +1,10 @@
+
+informar = {"name":"rayhan",
+            "age" :23}
+#print(informar)
+de = {'name': 'rayhan',
+      'list':[10,True,False,20,74,[3278,'madare'],],
+      'tuple':[29,8,'dklsaj']}
+#print(de)
+for d in de:
+    print(de[d])
